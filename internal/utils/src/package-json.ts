7216f331d7aba65a07d0841f45fe3e115ff010b9
safranyu <530548062@qq.com>
2025-03-03 17:50:46 +0800

    fix(ui): fix 修改报错

--- internal/utils/src/package-json.ts
+++ internal/utils/src/package-json.ts
@@ -3,7 +3,7 @@
 
 // TODO 这样写的好处是有类型，用 require 的写法是不是也可以手动指定类型呀，就是有点麻烦
 import rootPackageJson from '../../../package.json';
-import tdesignVueNextPackageJson from 'tdesign-vue-next/package.json';
-import tdesignVueNextSitePackageJson from 'tdesign-vue-next/site/package.json';
+import tdesignVueNextPackageJson from '@hlcloud/ui/package.json';
+import tdesignVueNextSitePackageJson from '@hlcloud/ui/site/package.json';
 
 export { rootPackageJson, tdesignVueNextPackageJson, tdesignVueNextSitePackageJson };
--- internal/utils/types/src/package-json.d.ts
+++ internal/utils/types/src/package-json.d.ts
@@ -1,4 +1,4 @@
 import rootPackageJson from '../../../package.json';
-import tdesignVueNextPackageJson from 'tdesign-vue-next/package.json';
-import tdesignVueNextSitePackageJson from 'tdesign-vue-next/site/package.json';
+import tdesignVueNextPackageJson from '@hlcloud/ui/package.json';
+import tdesignVueNextSitePackageJson from '@hlcloud/ui/site/package.json';
 export { rootPackageJson, tdesignVueNextPackageJson, tdesignVueNextSitePackageJson };
